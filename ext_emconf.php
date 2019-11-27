a42d07b99cbb4e49fc3225900d2d850a82a89a75
Lossy <Lossy@Gerald.homenet>
2019-11-27 02:08:49 +0000

    Restrict for Version Typo3 version 9 only

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '7.0.0-7.3.99',
-			'typo3' => '8.7.0-9.99.99',
+			'typo3' => '9.0.0-9.99.99',
 		),
 		'conflicts' => 
 		array (
