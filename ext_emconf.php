3c986151159ae4e26751b3660f0428833d0cd213
Lossy <Lossy@Gerald.homenet>
2019-11-27 23:03:09 +0000

    adjust supported version of PHP

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'php' => '7.0.0-7.3.99',
+			'php' => '7.2.0-7.3.99',
 			'typo3' => '9.0.0-9.99.99',
 		),
 		'conflicts' => 
